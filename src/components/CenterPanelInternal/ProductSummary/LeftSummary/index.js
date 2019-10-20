13719948364914767d25c453a904722cd8760495
anton-karlovskiy <antonkarlovskiy@outlook.com>
2019-10-20 22:48:15 +0300

    temporary storing in progress

--- src/components/CenterPanelInternal/ProductSummary/LeftSummary/index.js
+++ src/components/CenterPanelInternal/ProductSummary/LeftSummary/index.js
@@ -10,11 +10,41 @@ const ConditionPropertyValue = () => {
   );
 };
 
+// ray test touch <
+const USSizePropertyValue = () => {
+  return (
+    <select style={{
+      border: '1px solid lightgray',
+      width: '190px',
+      borderRadius: '3px'
+    }}>
+      <option value='select'>- Select -</option>
+      <option value='11' selected>11</option>
+    </select>
+  );
+};
+
+const upperPropertyFields = [
+  {
+    label: 'Condition',
+    value: <ConditionPropertyValue />
+  },
+  {
+    label: 'US Size',
+    value: <USSizePropertyValue />
+  }
+];
+// ray test touch >
+
 const LeftSummary = () => {
   return (
     <div className='generic-font-color'>
       <div className='product-name bold-font-color'>Nike Flex Run 2016 Men's Runinng Shoe Crimson/Black 830369-601 sz 11</div>
-      <PropertyField label='Condition' value={<ConditionPropertyValue />} />
+      {/* ray test touch < */}
+      { upperPropertyFields.map(propertyField => (
+        <PropertyField key={propertyField.label} label={propertyField.label} value={propertyField.value} />
+      )) }
+      {/* ray test touch > */}
     </div>
   );
 };
